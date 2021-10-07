3e59d23f7d3020569f24d386448db7d0bfcd93f7
Mark Angel <84360399+markangelhealingarts@users.noreply.github.com>
2021-10-06 23:54:51 -0700

    lab6 and lab6GUM

---
+++ ist541/lab6GUM/js/pain.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+// JavaScript Document
+$( function() {
+   $("map area").click( function(){
+
+      if ($(this).attr("id") == "Back Pain") {
+         $("#feedback").css("color","red");
+         $("#feedback").html("Back Pain and Pinched Nerves");
+      }
+
+      if ($(this).attr("id") == "Neck Pain") {
+         $("#feedback").css("color","green");
+         $("#feedback").html("Chronic Neck Pain");
+      }
+
+      if ($(this).attr("id") == "Chest Pain") {
+         $("#feedback").css("color","blue");
+         $("#feedback").html("Chest Pain and Cardiovascular Disease");
+      }
+
+  $("#feedback").css("backgroundColor","yellow");
+$('#myModal').modal("show");
+   });
+}); //end main jQuery function
