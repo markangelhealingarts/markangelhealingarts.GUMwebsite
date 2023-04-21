28143cfd535e220e5c53ae142580e662b2268b3b
ZihaoXu <zixu@csumb.edu>
2023-04-21 13:36:53 -0700

    update

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ let login_text = document.getElementById('login_text')
 let login_wrap = document.getElementById('login_wrap')
 
 if(localStorage.getItem('logged')){
-    login_wrap.innerHTML = `<a class="user_text" href="pages/profile.html">${localStorage.getItem('username')}</a>`
+    login_wrap.innerHTML = `<a class="user_text" href="../pages/profile.html">${localStorage.getItem('username')}</a>`
 }else{
-    login_wrap.innerHTML='<a class="user_text" href="pages/login.html">Login</a>'
+    login_wrap.innerHTML='<a class="user_text" href="../pages/login.html">Login</a>'
 }
