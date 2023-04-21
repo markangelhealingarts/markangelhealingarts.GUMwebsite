67930aa13e4959902ae8a96d7574f8c4770d8192
Jasmine Moua <jmoua@csumb.edu>
2023-04-21 00:19:11 -0700

    Alert

--- js/notifications.js
+++ js/notifications.js
@@ -45,4 +45,32 @@ loginButton.addEventListener("click", (e) => {
     }
 })
 
-setTimeout( function ( ) { alert( "moo" ); }, 10000 ); //displays msg in 10 seconds
+function getAlert() {
+  alert("Here is your hourly reminder to Get Up and Move!");
+  getTime();
+}    
+function getTime() {
+  let current = new Date();
+  let hour = current.getHours();
+  let minute = current.getMinutes();
+  let second = current.getSeconds();
+
+  console.log(current);
+
+  if ((minute == '00') && (second == '00')) {
+      if (hour == 9 || hour == 10 || hour == 11 || 
+      hour == 2 || hour == 3 || hour == 4) {
+          getAlert();
+      }
+  } else {
+      setTimeout(getAlert,(60*(60-minute)+(60-second))*1000);
+  }
+
+  if ((second == '00')) {
+      getAlert();
+  } else {
+      setTimeout(getAlert,(60*(60-minute)+(60-second))*1000);
+  }
+}
+
+myInterval = setInterval(getTime, 1000); //displays msg in 10 seconds
